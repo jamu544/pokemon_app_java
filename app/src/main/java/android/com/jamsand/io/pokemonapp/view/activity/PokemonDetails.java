90a5fa74c1d2893f87aa1e205c9f538084f73409
odwa <odwa.menziwa@jumpco.io>
2022-05-16 12:14:22 +0200

    cleaning code

--- app/src/main/java/android/com/jamsand/io/pokemonapp/view/activity/PokemonDetails.java
+++ app/src/main/java/android/com/jamsand/io/pokemonapp/view/activity/PokemonDetails.java
@@ -3,13 +3,39 @@ package android.com.jamsand.io.pokemonapp.view.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.com.jamsand.io.pokemonapp.R;
+import android.com.jamsand.io.pokemonapp.utils.AppConstants;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PokemonDetails extends AppCompatActivity {
+
+    private Context context;
+    private String pokemonName="";
+    private String pokemonID = "";
+
+    private TextView pokemonTextView;
+    private TextView weightTextView;
+    private TextView heightTextView;
+    private TextView typesTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_details);
+        context = this;
+
+        Intent intent = getIntent();
+         pokemonName = intent.getStringExtra(AppConstants.EXTRA_POKEMON_NAME);
+         pokemonID = intent.getStringExtra(AppConstants.EXTRA_POKEMON_ID);
+        init();
+    }
+
+    private void init(){
+        pokemonTextView = (TextView) findViewById(R.id.pokemonDetailsName);
+        weightTextView = (TextView)  findViewById(R.id.weightTextView);
+        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        typesTextView = (TextView) findViewById(R.id.typeTextView);
     }
 }
